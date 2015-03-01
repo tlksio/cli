22c14ef7a1e5855edbe35c6a07afe903f766eb52
Raül Pérez <repejota@gmail.com>
2015-03-01 18:49:17 +0100

    add cli --version --help

--- cli.js
+++ cli.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
+var argv = require('minimist')(process.argv.slice(2));
 
+// --version
+if (argv.version) {
+    var pkg = require('./package.json');
+    console.log(pkg.version);
+    process.exit();
+}
+
+console.log('usage: tlksio <options> <arguments>');
+console.log('');
+console.log('Options:');
+console.log('   --version   Show version number.');
+console.log('   --help      Show this help message.');
