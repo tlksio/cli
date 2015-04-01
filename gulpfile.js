9b2c6b779fd190a4b0f02710bce60449d4050b7c
Raül Pérez <repejota@gmail.com>
2015-04-01 02:45:41 +0200

    Added clean task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var del = require('del');
+
+gulp.task('clean', function() {
+    del(['node_modules', 'coverage'], function(err, delfiles) {
+        return err;
+    });
+});
 
 gulp.task('jshint', function() {
     gulp.src(['./lib/**/*.js', './*.js'])
