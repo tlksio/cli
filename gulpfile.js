e4866f57dff43db6dd4d0ec33443a5849e57bf18
Raül Pérez <repejota@gmail.com>
2015-04-01 02:23:33 +0200

    Created gulpfile with a jshint task

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('jshint', function() {
+    gulp.src(['./lib/**/*.js', './*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('default', ['jshint'], function() {});
